895f94679a23f3e7f937a2cfbfc88df0d7ff1115
NerdyStuff <NerdyStuff@users.noreply.github.com>
2020-03-26 12:51:50 +0100

    Refactoring Client; using DataPackages

--- SourceCode/Client/src/Client.java
+++ SourceCode/Client/src/Client.java
@@ -1,106 +1,227 @@
 import java.io.*;
 import java.net.*;
 import java.util.Date;
-import java.util.concurrent.TimeUnit;
+import java.util.List;
 
 public class Client {
 
-    private String hostname; //Hostname
-    private int port; //port
+    // TODO: List of Hostnames and ports
+    // TODO: setter for adding hostname and portpair
+    private String hostname = "localhost"; //Hostname
+    private int port = 1337; //port
     private String username, password;
 
-    public Client(String hostname, int port, String username, String password) {
+    public Client(String username, String password) {
         this.username = username;
         this.password = password;
-        this.hostname = hostname;
-        this.port = port;
     }
 
-    public void sendMessage(String receiver, String message) {
+    public String sendMessage(String receiver, String message) {
+        String returnString = "";
 
-        Socket socketToServer = null;
+        DataPackage dataPackage = new DataPackage(2, username, password, receiver, message, new Date());
+        int returnValue = sendDataPackage(dataPackage);
 
-        while (socketToServer != null) {
-            try {
-                socketToServer = new Socket(hostname, port);
-            } catch (UnknownHostException e) {
-                System.out.println("Error: Host not known");
+        // TODO : Error handling
+        /*switch (returnValue) {
+            case 0:
+                break;
+        }*/
 
-                // Sleep one second
-                try {
-                    TimeUnit.SECONDS.sleep(1);
-                } catch (InterruptedException interruptedExeption) {
-                    System.out.println("Error: Could not sleep for one second...");
+        // TODO: Retry + Error Handling
+        List<DataPackage> serverResponse = getDataPackageList(hostname, port);
+
+        if (serverResponse == null) {
+            // Error
+        } else {
+            if (!serverResponse.isEmpty()) {
+                DataPackage responsePackage = serverResponse.remove(0);
+
+                int responseFlag = responsePackage.getFlag();
+
+                switch (responseFlag) {
+                    case 7:
+                        returnString = "Message accepted";
+                        break;
+                    case -2:
+                        returnString = "Wrong Username or Password";
+                        break;
+                    case -3:
+                        returnString = "Receiver does not exists";
+                        break;
+                    case -4:
+                        returnString = "An error occured";
+                        break;
+                    case -5:
+                        returnString = "You can not be the receiver";
+                        break;
+
+                    default:
+                        returnString = "An error occured";
                 }
 
-            } catch (IOException e) {
-                System.out.println("Error: IOException");
-
-                // Sleep one second
-                try {
-                    TimeUnit.SECONDS.sleep(1);
-                } catch (InterruptedException interruptedExeption) {
-                    System.out.println("Error: Could not sleep for one second...");
-                }
+            } else {
+                // error
             }
         }
 
-        if(socketToServer != null) {
 
-            try {
-                ObjectOutputStream objectOutputStream = new ObjectOutputStream(socketToServer.getOutputStream());
-
-                DataPackage dataPackage = new DataPackage(2, username, password, receiver, message,  new Date());
-
-                objectOutputStream.writeObject(dataPackage);
-            } catch (IOException e) {
-                System.out.println("Error: Could not send data...");
-                e.printStackTrace();
-            }
-        }
+        return returnString;
     }
 
     public void getUpdates() {
-/*
-        Socket socketToServer = null;
 
-        while (socketToServer != null) {
-            try {
-                socketToServer = new Socket(hostname, port);
-            } catch (UnknownHostException e) {
-                System.out.println("Error: Host not known");
+    }
 
-                // Sleep one second
-                try {
-                    TimeUnit.SECONDS.sleep(1);
-                } catch (InterruptedException interruptedExeption) {
-                    System.out.println("Error: Could not sleep for one second...");
+    public String register() {
+        String returnString = "";
+
+        DataPackage dataPackage = new DataPackage(1, username, password);
+        int returnValue = sendDataPackage(dataPackage);
+
+        // TODO : Error handling
+        /*switch (returnValue) {
+            case 0:
+                break;
+        }*/
+
+        // TODO: Retry + Error Handling
+        List<DataPackage> serverResponse = getDataPackageList(hostname, port);
+
+        if (serverResponse == null) {
+            // Error
+        } else {
+            if (!serverResponse.isEmpty()) {
+                DataPackage responsePackage = serverResponse.remove(0);
+
+                int responseFlag = responsePackage.getFlag();
+
+                switch (responseFlag) {
+                    case -1:
+                        returnString = "Registration failed";
+                        break;
+                    case -6:
+                        returnString = "Wrong Username or Password";
+                        break;
+
+                    default:
+                        returnString = "An error occured";
                 }
 
-            } catch (IOException e) {
-                System.out.println("Error: IOException");
-
-                // Sleep one second
-                try {
-                    TimeUnit.SECONDS.sleep(1);
-                } catch (InterruptedException interruptedExeption) {
-                    System.out.println("Error: Could not sleep for one second...");
-                }
+            } else {
+                // error
             }
         }
 
-        if(socketToServer != null) {
+        return returnString;
+    }
+
+    private List<DataPackage> getDataPackageList(String hostname, int port) {
+
+        Socket socketFromServer = null;
+        List<DataPackage> data = null;
+        boolean errorOccured = false;
+
+        try {
+            socketFromServer = new Socket(hostname, port);
+        } catch (UnknownHostException e) {
+            System.out.println("Error: Host not known");
+
+            return null;
+
+        } catch (IOException e) {
+            System.out.println("Error: IOException");
+
+            return null;
+        }
+
+        if (socketFromServer != null) {
+
+            try {
+                InputStream inputStream = socketFromServer.getInputStream();
+
+                // create a DataInputStream so we can read data from it.
+                ObjectInputStream objectInputStream = null;
+                Object object = null;
+
+                try {
+                    objectInputStream = new ObjectInputStream(inputStream);
+                } catch (StreamCorruptedException e) {
+                    errorOccured = true;
+                    System.out.println("Error: StreamCorruptedException");
+                }
+
+                if (!errorOccured) {
+                    try {
+                        object = objectInputStream.readObject();
+                    } catch (NullPointerException e) {
+                        errorOccured = true;
+                        System.out.println("Error: NullPointerException");
+                    }
+                }
+
+                if (!errorOccured && object != null) {
+                    if (object instanceof DataPackage) {
+                        data = (List<DataPackage>) object;
+                    } else {
+                        throw new Exception("Error: Not a valid DataPackage-List");
+                    }
+                } else {
+                    System.out.println("Error: Object is null");
+                }
+
+            } catch (StreamCorruptedException e) {
+                System.out.println("Error: Did not receive an object...");
+                e.printStackTrace();
+            } catch (IOException e) {
+                System.out.println("Error: Couldn't get data from socket...");
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                System.out.println("Error: Class not found...");
+                e.printStackTrace();
+            } catch (Exception e) {
+                System.out.println("Error: An error occured...");
+                e.printStackTrace();
+            }
+        }
+
+        return data;
+    }
+
+    private int sendDataPackage(DataPackage dataPackage) {
+
+        int returnFlag = 0;
+        Socket socketToServer = null;
+
+
+        try {
+            socketToServer = new Socket(hostname, port);
+        } catch (UnknownHostException e) {
+            System.out.println("Error: Host not known");
+
+            return -1; //unknown Host
+
+        } catch (IOException e) {
+            System.out.println("Error: IOException");
+
+            return -2; //IOException
+        }
+
+
+        if (socketToServer != null) {
 
             try {
                 ObjectOutputStream objectOutputStream = new ObjectOutputStream(socketToServer.getOutputStream());
-
-                DataPackage dataPackage = new DataPackage(2, username, password  new Date());
 
                 objectOutputStream.writeObject(dataPackage);
             } catch (IOException e) {
                 System.out.println("Error: Could not send data...");
                 e.printStackTrace();
+
+                return -3; // Could not send Data
             }
-        }*/
+        }
+
+        return returnFlag;
     }
 }
